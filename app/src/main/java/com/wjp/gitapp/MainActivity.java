c3c4cfcc8301c2a0377485fd88ac3c9a84b27465
Jiapan Wang <jiwa119e@tu-dresden.de>
2022-11-01 07:51:25 +0100

    jiapan's initial commit

---
+++ app/src/main/java/com/wjp/gitapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.wjp.gitapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
